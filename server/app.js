caf67e549f0a495228bc7e74f0ae1ba06fa41688
Nir Geron <nirb2000@gmail.com>
2025-06-17 16:48:10 +0300

    Update corsOptions inside app.js

--- server/app.js
+++ server/app.js
@@ -50,19 +50,26 @@ const allowedOrigins = [
   "https://spotly-one.vercel.app",
 ];
 
-// Enabling CORS (Cross-Origin Resource Sharing)
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: function (origin, callback) {
+    const allowedOrigins = [
+      "http://localhost:3000",
+      "https://spotly-one.vercel.app",
+    ];
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+
+app.options("*", cors(corsOptions));
 
 // Set security HTTP headers
 app.use(helmet());
