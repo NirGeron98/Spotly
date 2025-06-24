b2123087aa2e4a52ba378fb7c8e15fb769789f37
Nir Geron <nirb2000@gmail.com>
2025-06-24 18:04:55 +0300

    change parking result card ui

--- client/src/components/search-private-parking/ParkingResultsGrid.js
+++ client/src/components/search-private-parking/ParkingResultsGrid.js
@@ -9,7 +9,7 @@ const ParkingResultsGrid = ({
   formatAddress,
 }) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-7xl mx-auto">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-5xl mx-auto">
       {parkingSpots.map((spot) => (
         <ParkingSpotCard
           key={spot._id}
